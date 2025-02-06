7845e3ffa1728437c6500a64209798eb570be4f7
“Daniel <danielmmckean@gmail.com>
2025-02-06 15:03:49 +0000

    forgot to commit at the end of chapter 5 so commting now

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+#username for superuser is 2895695M and password is my uni login password
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
